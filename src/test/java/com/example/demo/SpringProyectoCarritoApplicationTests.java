6687f2262c76d6a168e972c0af428238a3736ad6
macabros554 <macabros554@gmail.com>
2022-02-15 11:04:01 +0100

    continuacion

--- src/test/java/com/example/demo/SpringProyectoCarritoApplicationTests.java
+++ src/test/java/com/example/demo/SpringProyectoCarritoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.Usuario;
 
 @SpringBootTest
 class SpringProyectoCarritoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+Usuario usuario;
+	
+	
+	@BeforeEach
+	void init() {
+		usuario= new Usuario("ew", "ew", "ew", "ew", "ew", "ew");
+	}
+	
+	@Test
+	void test() {
+		assertEquals("ew2", usuario.getDireccion());
 	}
 
 }
